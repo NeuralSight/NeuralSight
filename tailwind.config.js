3952b5efe2bfa7708a0de43b76db6d4652d83e9e
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-15 12:32:35 +0300

    tailwind configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          lightest: '#E3F4F6',
+          light: '#16C2D5',
+          dark: '#10217D',
+        },
+        accent: '#D7BAAD',
+      },
+    },
+  },
+  plugins: [],
+}
