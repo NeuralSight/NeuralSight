e78d4ebc6099f7d8115864fa2af9f2d7ae5265bd
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-15 13:33:21 +0300

    page index files entry page

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Dashboard: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Neural Sight</title>
+        <meta name='description' content='Neural Sight an Ai medical system' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className=''>Hello World</div>
+    </div>
+  )
+}
+
+export default Dashboard
