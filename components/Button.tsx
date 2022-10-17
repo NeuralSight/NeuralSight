31281ad06ecc3874ef6c8b226503d2d2e0083ddd
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-17 16:42:37 +0300

    update btn behaviour added disabling feature

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,19 @@ type Props = {
   children: React.ReactNode
   outlined: boolean | undefined
   type: 'submit' | 'button' | 'reset' | undefined
+  disable: boolean
 }
 
 const Button = (props: Props) => {
   return (
     <button
       type={props.type}
-      className={`w-full max-h-[24px] flex cursor-pointer rounded-xl border border-primary-light hover:border-primary-dark bg-primary-light hover:bg-primary-dark py-6 px-24 items-center justify-center transition-all duration-200 ease-in-out shadow-md shadow-primary-light/25 hover:shadow-primary-dark/25 hover:shadow text-white font-bold text-sm lg:text-base capitalize`}
+      disabled={props.disable}
+      className={`w-full max-h-[24px] flex cursor-pointer rounded-xl border ${
+        props.disable
+          ? 'bg-primary-light/50 border-primary-light/30 text-gray-100 shadow-none'
+          : 'border-primary-light hover:border-primary-dark bg-primary-light hover:bg-primary-dark text-white shadow-md shadow-primary-light/25 hover:shadow-primary-dark/25 hover:shadow transition-all duration-200 ease-in-out'
+      }  py-6 px-24 items-center justify-center font-bold text-sm lg:text-base capitalize`}
     >
       {props.children}
     </button>
