7d2c7d4a78f7544abd4a9d0c6571cef127009930
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-21 16:08:33 +0300

    login hook update

--- hooks/use-login.ts
+++ hooks/use-login.ts
@@ -4,32 +4,34 @@ type AuthUser = {
   grant_type?: 'password' | string
   username: string
   password: string
-  scope: string | null | undefined
-  client_id: string | null | undefined
-  client_secret: string | null | undefined
+  scope?: '' | string | null | undefined
+  client_id?: string | null | undefined
+  client_secret?: string | null | undefined
 }
 
 const headers = new Headers({ 'Content-Type': 'application/json' })
 
 const loginUser = async (user: AuthUser) => {
+  // console.log('user', user)
   const response = await fetch(
-    `${process.env.NEURALSIGHT_API_BASE_URL}/v1/login/access-token`,
+    `${process.env.NEXT_PUBLIC_NEURALSIGHT_API_BASE_URL}/v1/login/access-token`,
     {
+      mode: 'cors',
       method: 'POST',
       headers: headers,
       body: JSON.stringify(user),
     }
   )
   const data = await response.json()
-  console.log('data', data)
-  return data
+  // console.log('data', data)
+  return { data, status: response.status }
 }
 
 export default function useLogin() {
   const currentClient = useQueryClient()
   const onSubmit = useMutation(loginUser, {
     onSuccess(data) {
-      console.log('data', data)
+      // console.log('data', data)
       currentClient.invalidateQueries(['user'])
     },
     onError(err) {
