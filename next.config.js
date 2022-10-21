bb9b790d17386ffc4f6d090d59f29d612b41206b
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-21 16:06:06 +0300

    remove env from the config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEURALSIGHT_API_BASE_URL: 'https://prod.api.neurallabs.africa/api',
-  },
   reactStrictMode: true,
   swcMinify: true,
 }
