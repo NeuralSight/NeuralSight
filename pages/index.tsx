2d57c59415eec848682225d291449f73dd6ea7cc
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-23 10:25:20 +0300

    added sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { lazy } from 'react'
+import SideBar from '../components/SideBar'
 
 const Dashboard: NextPage = () => {
   return (
-    <div>
+    <div className='max-h-screen  h-screen w-full bg-primary-dark'>
       <Head>
         <title>Neural Sight</title>
         <meta name='description' content='Neural Sight an Ai medical system' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div className=''>Hello World</div>
+      <div className='h-full w-full flex justify-between'>
+        <SideBar />
+      </div>
     </div>
   )
 }
