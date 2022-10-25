aaf737d3ead93cd8b1b77236eb453322f8a8858a
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-26 00:31:27 +0300

    intial main content commit

---
+++ components/dashboard/MainContentSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const MainContentSection = (props: Props) => {
+  return <div className='w-full h-full '>MainContentSection</div>
+}
+
+export default MainContentSection
