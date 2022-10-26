ff46da1b6b71c9e21c67f2482103a06407c57913
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-26 20:47:44 +0300

    neural labs text logo

---
+++ components/NeuralLabsTextLogo.tsx
@@ -0,0 +1,13 @@
+import { Icon } from '@iconify/react'
+import React from 'react'
+
+const NeuralLabsTextLogo = () => {
+  return (
+    <h2 className='font-black text-lg md:text-xl tracking-widest capitalize text-primary-dark my-auto align-middle text-centez flex'>
+      Neural Sight{' '}
+      <Icon icon={'emojione:eye-in-speech-bubble'} className='h-8 w-8 ml-2' />
+    </h2>
+  )
+}
+
+export default NeuralLabsTextLogo
