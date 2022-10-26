eba468647fdf194d3ef6a03e5b4d9fc7f44583bf
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-26 20:47:04 +0300

    added toggle buttons

---
+++ components/ViewToggleBtn.tsx
@@ -0,0 +1,38 @@
+import { Icon } from '@iconify/react'
+import React from 'react'
+
+type Props = {
+  isListView: boolean
+  setIsListView: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ViewToggleBtn = (props: Props) => {
+  return (
+    <div className='flex justify-center items-center space-x-1'>
+      <button
+        onClick={() => props.setIsListView(false)}
+        className='flex justify-center items-center p-1 hover:bg-primary-light/20 rounded transition-all duration-200 ease-in-out'
+      >
+        <Icon
+          icon='carbon:grid'
+          className={`h-8 w-8 fill-current stroke-[3px] ${
+            !props.isListView ? 'text-primary-light' : 'text-gray-600'
+          }`}
+        />
+      </button>
+      <button
+        onClick={() => props.setIsListView(true)}
+        className='flex justify-center items-center p-1 hover:bg-primary-light/20 rounded transition-all duration-200 ease-in-out'
+      >
+        <Icon
+          icon='bi:list-task'
+          className={`h-8 w-8 fill-current stroke-[3px]  ${
+            props.isListView ? 'text-primary-light' : 'text-gray-600'
+          }`}
+        />
+      </button>
+    </div>
+  )
+}
+
+export default ViewToggleBtn
