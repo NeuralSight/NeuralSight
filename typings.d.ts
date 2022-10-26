4d4904e60022ef872c7dbf73937098235d4c0238
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-27 01:01:05 +0300

    added Image Details type

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,15 @@
+// ImageDetails
 
+interface Pathogen {
+  type: string
+  confidence: number | string | undefined
+}
+export interface ImageDetails {
+  patientID: string
+  modality: string
+  src: string | import('../dist/client/image').StaticImageData
+  disease: string
+  inference: number | string
+  pathogens: Pathogen[]
+  totalPathogens: string | number
+}
