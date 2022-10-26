b9e9f9810dfa1fe70386be59cacf06d3566a9a73
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-26 20:44:35 +0300

    introduced custom scrollbar

--- components/dashboard/PatientIdCard.tsx
+++ components/dashboard/PatientIdCard.tsx
@@ -9,27 +9,30 @@ type Props = {
 }
 
 const PatientIdCard = (props: Props) => {
-  console.log('active', props.active)
   return (
     <div
-      className={`cursor-pointer relative ${
-        props.active && 'font-semibold'
-      } py-2 px-6 text-center rounded-full hover:bg-primary-light/20`}
+      className={`cursor-pointer relative  `}
       onClick={() => props.handleActive(props.idKey)}
     >
-      {props.id}
       <div
-        className={`flex bg-primary-dark rounded-tl-xl rounded-bl-xl absolute -right-9 items-center top-1/2 transform -translate-y-1/2 border-l-2 border-t-2 border-b-2 border-primary-light`}
+        className={`${
+          props.active && 'font-semibold '
+        } mx-[26px] py-2 px-6 text-center rounded-full hover:bg-primary-light/20`}
       >
-        {props.active ? (
+        {props.id}
+      </div>
+      {props.active ? (
+        <div
+          className={`flex bg-primary-dark rounded-tl-xl rounded-bl-xl absolute -left-1 items-center top-1/2 transform rotate-180 -translate-y-1/2 border-l-2 border-t-2 border-b-2 border-primary-light`}
+        >
           <Icon
             icon={'eva:arrow-ios-back-outline'}
             className='h-7 w-7 fill-current text-gray-50'
           />
-        ) : (
-          <></>
-        )}
-      </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
--- components/dashboard/PatientIdSection.tsx
+++ components/dashboard/PatientIdSection.tsx
@@ -17,49 +17,58 @@ const patientIDs = [
   '600d475fa96e305as2e48c9cfbb851qs',
   '00d475fa96e305as2e48c9cfbb851qs',
   '600d475fa96e305as2e48c9cfbb851qs',
+  '6440d475s96e305as2e48c9cfbb851qs',
+  '600d475fa96e305as2e48c9cfbb851qs',
+  '600d475fa96e305as2e48c9cfbb851qs',
+  '00d475fa96e305as2e48c9cfbb851qs',
+  '600d475fa96e305as2e48c9cfbb851qs',
 ]
 
 function PatientIdSection({}: Props) {
   const [active, setActive] = React.useState<number>(0)
   return (
-    <div className='w-full h-full border-2 border-primary-light rounded-[25px] bg-gray-50 py-5 px-3 flex flex-col space-y-6'>
-      <InputField
-        id='search'
-        type='text'
-        placeholder='Search Patient Id...'
-        size='small'
-        label='search'
-        borderRadius='25px'
-        icon={
-          <Icon
-            icon='ep:search'
-            className='cursor-pointer w-6 h-6 fill-current text-zinc-500/50'
-          />
-        }
-      />
-      <InputField
-        id='search'
-        type='text'
-        size='small'
-        label='New ID'
-        borderRadius='25px'
-        icon={
-          <button
-            type='submit'
-            className='absolute rounded-[25px] bg-gray-500/70 group-focus:group-hover:bg-primary-light flex justify-center items-center p-1.5 right-1 transition-all duration-200'
-          >
+    <div className='w-full h-full border-2 border-primary-light rounded-2xl bg-gray-50/95 py-5 flex flex-col space-y-6 '>
+      <div className='flex flex-col space-y-6 px-3'>
+        <InputField
+          id='search'
+          type='text'
+          placeholder='Search Patient Id...'
+          size='small'
+          label='search'
+          borderRadius='25px'
+          icon={
             <Icon
-              icon='akar-icons:person-add'
-              className=' cursor-pointer w-5 h-5 fill-current text-gray-50'
+              icon='ep:search'
+              className='cursor-pointer w-6 h-6 fill-current text-zinc-500/50'
             />
-          </button>
-        }
-      />
-      <div className='flex flex-col space-y-6 pr-6'>
+          }
+        />
+        <InputField
+          id='search'
+          type='text'
+          size='small'
+          label='New ID'
+          borderRadius='25px'
+          icon={
+            <button
+              type='submit'
+              className='absolute rounded-[25px] bg-gray-500/70 group-focus:group-hover:bg-primary-light flex justify-center items-center p-1.5 right-1 transition-all duration-200'
+              title='add patient'
+            >
+              <Icon
+                aria-label='add patient using Patients Id'
+                icon='akar-icons:person-add'
+                className=' cursor-pointer w-5 h-5 fill-current text-gray-50'
+              />
+            </button>
+          }
+        />
+      </div>
+      <div className='flex flex-col space-y-6 '>
         <div className='uppercase text-lg text-center underline underline-offset-2'>
-          patient id
+          patient&#39;s id
         </div>
-        <div className='flex flex-col space-y-3 '>
+        <div className='flex flex-col space-y-3 overflow-y-scroll max-h-[480px] w-full scrollbar-thin scrollbar-thumb-primary-light scrollbar-track-primary-light/10 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scroll-smooth'>
           {patientIDs.map((patientId, key) => (
             <PatientIdCard
               id={patientId}
