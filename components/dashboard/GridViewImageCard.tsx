363f07d8b0926c0514e7f006d654c49993618d45
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-28 19:07:54 +0300

    updated gridview styles

--- components/dashboard/GridViewImageCard.tsx
+++ components/dashboard/GridViewImageCard.tsx
@@ -50,7 +50,7 @@ const GrdViewImageCard = ({ imageDetails }: Props) => {
 
       <div
         className={
-          'rounded-full py-3 w-full opacity-100 ease-in-out transition-opacity duration-300  h-fit absolute bottom-0  right-0 flex items-center justify-center text-center align-middle bg-primary-light/40 font-bold text-gray-900 text-sm'
+          'rounded-full py-3 w-full opacity-100 ease-in-out transition-opacity duration-300  h-fit absolute bottom-0  right-0 flex items-center justify-center text-center align-middle bg-primary-light/40 font-bold text-primary-dark text-sm'
         }
       >
         {/* show disease likelihood as summary maybe later it would be a bottomsheet for small devices for people to pull more info */}
@@ -61,7 +61,7 @@ const GrdViewImageCard = ({ imageDetails }: Props) => {
           ${
             imageDetails.inference < 0.5
               ? 'text-green-700'
-              : imageDetails.inference > 0.5
+              : imageDetails.inference > 0.75
               ? 'text-red-700'
               : 'text-orange-700'
           }`}
