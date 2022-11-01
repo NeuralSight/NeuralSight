74938fea415ce38d62407d1c35fa78cbe73dc340
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-01 15:37:42 +0300

    changed font

--- components/NeuralLabsTextLogo.tsx
+++ components/NeuralLabsTextLogo.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 const NeuralLabsTextLogo = () => {
   return (
-    <h2 className='font-medium text-sm md:text-xl tracking-widest uppercase text-primary-dark my-auto align-middle text-centez flex'>
+    <h2
+      style={{ fontFamily: 'Oswald' }}
+      className='font-medium text-sm md:text-xl tracking-widest uppercase text-primary-dark my-auto align-middle text-centez flex'
+    >
       Neural Sight{' '}
       <Icon icon={'emojione:eye-in-speech-bubble'} className='h-8 w-8 ml-2' />
     </h2>
