b7e7a727920eef79dabda94c13cfe8b7cd4769d6
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-03 16:05:39 +0300

    changed the design

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -7,7 +7,7 @@ type Props = {
 const ErrorMessage = (props: Props) => {
   return (
     <p
-      className={`w-full py-3 px-6 rounded-lg bg-red-400/40 text-red-500 font-medium transition-all duration-300 ease-in-out`}
+      className={`w-full py-3 px-6 text-white bg-red-500/90 lg:text-lg font-medium tracking-wider transition-all duration-300 ease-in-out text-center`}
     >
       {props.children}
     </p>
