96f502024270faf610659a6f34bac416b731861d
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-03 16:05:24 +0300

    added additional props for flexiblity

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,26 +5,35 @@ type Props = {
   outlined?: boolean | undefined
   type?: 'submit' | 'button' | 'reset' | undefined
   disable?: boolean
+  size?: string | number | undefined
+  textSize?: 'text-sm' | 'text-md' | 'text-lg' | string | undefined
+  textBold?: 'font-bold' | 'font-semibold' | 'font-medium' | 'font-normal'
 }
 
-const Button = (props: Props) => {
+const Button = ({
+  children,
+  type,
+  disable,
+  outlined,
+  size,
+  textBold = 'font-bold',
+  textSize = 'text-sm',
+}: Props) => {
   return (
     <button
-      type={props.type}
-      disabled={props.disable}
-      className={`w-full max-h-[24px] flex items-center justify-center cursor-pointer rounded-xl  fill-current ${
-        props.disable
+      type={type}
+      disabled={disable}
+      className={`w-full max-h-[24px] flex items-center justify-center cursor-pointer rounded-xl fill-current border-2 ${
+        disable
           ? 'bg-primary-light/50 border-primary-light/30 text-gray-100 shadow-none'
-          : `  ${
-              props.outlined ? 'border-2' : 'border'
-            } border-primary-light hover:border-primary-dark ${
-              props.outlined
+          : `   border-primary-light hover:border-primary-dark ${
+              outlined
                 ? 'bg-transparent text-primary-light'
-                : 'bg-primary-light text-white'
-            } hover:bg-primary-dark hover:text-white shadow-md shadow-primary-light/25 hover:shadow-primary-dark/25 hover:shadow transition-all duration-200 ease-in-out`
-      }  py-6 font-bold text-sm lg:text-base capitalize`}
+                : 'shadow-md  shadow-primary-light/25 hover:shadow-primary-dark/25 bg-primary-light text-white'
+            } hover:bg-primary-dark hover:text-white  hover:shadow transition-all duration-200 ease-in-out`
+      }  py-6 ${textBold} ${textSize} lg:text-base capitalize`}
     >
-      {props.children}
+      {children}
     </button>
   )
 }
