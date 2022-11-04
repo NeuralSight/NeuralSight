3f910078a2cc06ec21619c0e0f66089da8427876
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-04 18:47:09 +0300

    added menu array to helper folder

---
+++ helper/menu.ts
@@ -0,0 +1,26 @@
+export const Menu = [
+  {
+    key: 1,
+    text: 'home',
+    icon: 'ci:home-fill',
+    link: '/',
+  },
+  {
+    key: 2,
+    text: 'report',
+    icon: 'ant-design:pie-chart-filled',
+    link: '/report',
+  },
+  {
+    key: 3,
+    text: 'setting',
+    icon: 'ant-design:setting-filled',
+    link: '/setting',
+  },
+  {
+    key: 4,
+    text: 'alert',
+    icon: 'clarity:notification-solid',
+    link: '/alert',
+  },
+]
