3f3b6a22828cc32f2d899fadaddaaf33697c828e
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-04 18:52:41 +0300

    refactored code

--- components/NeuralLabsTextLogo.tsx
+++ components/NeuralLabsTextLogo.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 
 const NeuralLabsTextLogo = () => {
   return (
-    <h2
-      style={{ fontFamily: 'Oswald' }}
-      className='font-medium text-sm md:text-xl tracking-widest uppercase text-primary-dark my-auto align-middle text-centez flex'
-    >
-      Neural Sight{' '}
+    <div className='flex space-x-1 items-center'>
+      <h2
+        style={{ fontFamily: 'Oswald' }}
+        className='font-medium text-sm md:text-xl tracking-widest uppercase text-primary-dark my-auto align-middle text-centez flex'
+      >
+        Neural Sight{' '}
+      </h2>
       <Icon icon={'emojione:eye-in-speech-bubble'} className='h-8 w-8 ml-2' />
-    </h2>
+    </div>
   )
 }
 
