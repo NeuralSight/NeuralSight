b4030afccd0f58ec3d43790ab24887614c3970c7
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-04 18:46:14 +0300

    added footer and started working on responsiveness on different devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,59 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import SideBar from '../components/SideBar'
 import PatientIDSection from '../components/dashboard/PatientIdSection'
 import MainContentSection from '../components/dashboard/MainContentSection'
+import { useMediaQuery } from 'react-responsive'
+import { SCREEN } from '../helper/responsive'
+import SmallDeviceNavBar from '../components/SmallDeviceNavBar'
 
 const Dashboard: NextPage = () => {
+  const isLargeDevice = useMediaQuery({ minWidth: SCREEN.lg })
   return (
-    <div className='min-h-screen h-fit w-full bg-primary-dark'>
+    <div className='relative h-full lg:min-h-screen lg:h-fit w-full bg-primary-dark mx-auto'>
       <Head>
         <title>Neural Sight</title>
         <meta name='description' content='Neural Sight an Ai medical system' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <div className='h-[inherit] w-full flex justify-between '>
-        <SideBar />
+      {!isLargeDevice && (
+        <header className='w-full h-fit'>
+          <SmallDeviceNavBar />
+        </header>
+      )}
+      <div className='h-full w-full flex justify-between '>
+        {isLargeDevice && <SideBar />}
         <main
           className='w-full h-full flex pt-6
-        pb-12 px-8 gap-8 '
+        pb-10 justify-evenly'
         >
-          <section className='w-[30%] '>
+          <section className='hidden lg:block w-100'>
             <PatientIDSection />
           </section>
-          <section className='w-[70%]'>
+          <section className='w-100'>
             <MainContentSection />
           </section>
         </main>
       </div>
+      {/* copyright */}
+      <footer className='absolute bottom-[2.5%] flex w-full space-x-2 justify-center items-center'>
+        <p className=' text-sm lg:text-base text-slate-300 '>
+          &#169; Neurallabs Africa
+        </p>
+        <div className='rounded-full h-1 w-1 bg-white/80' />
+        <Link href={'#'}>
+          <p className='text-sm lg:text-base text-slate-300 cursor-pointer hover:text-slate-100 hover:underline hover:decoration-slate-100'>
+            Contact us
+          </p>
+        </Link>
+        <div className='rounded-full h-1 w-1 bg-white/80' />
+        <Link href={'#'}>
+          <p className='text-sm lg:text-base text-slate-300 cursor-pointer hover:text-slate-100 hover:underline hover:decoration-slate-100'>
+            Terms & privacy
+          </p>
+        </Link>
+      </footer>
     </div>
   )
 }
