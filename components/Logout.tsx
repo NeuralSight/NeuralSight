ab9e6b3b69986e5411d5a3487efc9d93b4bef500
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-05 10:07:38 +0300

    nothing changed

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -6,9 +6,9 @@ type Props = {}
 const Logout = (props: Props) => {
   return (
     <button
-      className={` cursor-pointer px-6 rounded-xl hover:bg-primary-light/20 fill-currenthover:text-primary-light/80 flex-col flex justify-center items-center space-y-2 py-[1rem] text-gray-50`}
+      className={`cursor-pointer px-6 rounded-xl hover:bg-primary-light/20 fill-currenthover:text-primary-light/80 flex-col flex justify-center items-center space-y-2 py-[1rem] text-gray-50`}
     >
-      <Icon icon={'uiw:logout'} className={` w-8 h-8`} />
+      <Icon icon={'uiw:logout'} className={`lg:w-8 lg:h-8 fill-current`} />
 
       <p className='text-sm lowercase'>logout</p>
     </button>
