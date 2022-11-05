7083f447eacb807de9ab9ae1c471c6e3da576153
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-05 10:07:03 +0300

    added small device menu

--- components/SmallDeviceNavBar.tsx
+++ components/SmallDeviceNavBar.tsx
@@ -1,13 +1,31 @@
+import { useState, MouseEvent } from 'react'
 import NeuralLabsTextLogo from './NeuralLabsTextLogo'
 import Profile from './Profile'
+import Menu from './small-device-menu-popover'
 
 type Props = {}
 
 const SmallDeviceNavBar = (props: Props) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  // poppover props
+  const open = Boolean(anchorEl)
+  const id = open ? 'menu' : undefined
   return (
     <nav className='h-fit w-full flex justify-between bg-slate-50 items-center px-2 py-2 border-b-2 border-primary-light'>
       <NeuralLabsTextLogo />
-      <Profile />
+      <button onClick={handleClick}>
+        <Profile />
+      </button>
+      <Menu
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        setAnchorElement={setAnchorEl}
+      />
     </nav>
   )
 }
