45ddc324c87e81542dfbaf1bd968ada52343fe33
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-05 13:08:07 +0300

    menu style

---
+++ styles/menu.style.ts
@@ -0,0 +1,33 @@
+export const bugerStyles = {
+  // bmBurgerButton: {
+  //   position: 'relative',
+  //   width: '48px',
+  //   height: '48px',
+  //   left: '0px',
+  //   top: '0px',
+  // },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100vh',
+    top: '0',
+    left: '0',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)',
+    position: 'fixed',
+    top: '0',
+  },
+  bmMenu: {
+    background: '#fafafa',
+    padding: '1.25rem 0',
+    fontSize: '1.15em',
+  },
+
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em 0',
+  },
+  bmItem: {
+    display: 'inline-block',
+  },
+}
