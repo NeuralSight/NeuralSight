ea25df59ee5e1e636eca064dd357802efc7232cb
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-07 17:32:00 +0300

    seperated layout from dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import SideBar from '../components/SideBar'
 import PatientIDSection from '../components/dashboard/PatientIdSection'
 import MainContentSection from '../components/dashboard/MainContentSection'
-import { useMediaQuery } from '@mui/material'
-import { SCREEN } from '../helper/responsive'
-import SmallDeviceNavBar from '../components/SmallDeviceNavBar'
+import Layout from './layout'
 
-const Dashboard: NextPage = () => {
-  const isLargeDevice = useMediaQuery(`( min-width: ${SCREEN.lg} )`)
+type Props = {}
+
+function index({}: Props) {
   return (
-    <div className='relative h-full lg:min-h-screen lg:h-fit w-full bg-primary-dark mx-auto'>
-      <Head>
-        <title>Neural Sight</title>
-        <meta name='description' content='Neural Sight an Ai medical system' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      {isLargeDevice || (
-        <header className='w-full h-fit'>
-          <SmallDeviceNavBar />
-        </header>
-      )}
-      <div className='h-full w-full flex justify-between '>
-        {isLargeDevice && <SideBar />}
-        <main
-          className='w-full h-full flex pt-6
-        pb-10 justify-evenly'
-        >
-          <section className='hidden lg:block w-full lg:w-auto xl:w-auto'>
-            <PatientIDSection />
-          </section>
-          <section className='w-full lg:w-[55%] xl:w-[70%]'>
-            <MainContentSection />
-          </section>
-        </main>
-      </div>
-      {/* copyright */}
-      <footer className='absolute bottom-[0.8%] xl:bottom-[2.5%] flex w-full space-x-2 justify-center items-center'>
-        <p className=' text-sm lg:text-base text-slate-300 '>
-          &#169; Neurallabs Africa
-        </p>
-        <div className='rounded-full h-1 w-1 bg-white/80' />
-        <Link href={'#'}>
-          <p className='text-sm lg:text-base text-slate-300 cursor-pointer hover:text-slate-100 hover:underline hover:decoration-slate-100'>
-            Contact us
-          </p>
-        </Link>
-        <div className='rounded-full h-1 w-1 bg-white/80' />
-        <Link href={'#'}>
-          <p className='text-sm lg:text-base text-slate-300 cursor-pointer hover:text-slate-100 hover:underline hover:decoration-slate-100'>
-            Terms & privacy
-          </p>
-        </Link>
-      </footer>
-    </div>
+    <Layout>
+      <main className='w-full h-full flex pt-6 pb-10 justify-evenly'>
+        <section className='hidden lg:block w-full lg:w-auto xl:w-auto'>
+          <PatientIDSection />
+        </section>
+        <section className='w-full lg:w-[55%] xl:w-[70%]'>
+          <MainContentSection />
+        </section>
+      </main>
+    </Layout>
   )
 }
 
-export default Dashboard
+export default index
---
+++ pages/layout.tsx
@@ -0,0 +1,54 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useMediaQuery } from '@mui/material'
+import SideBar from '../components/SideBar'
+
+import { SCREEN } from '../helper/responsive'
+import SmallDeviceNavBar from '../components/SmallDeviceNavBar'
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+const Layout = ({ children }: Props) => {
+  const isLargeDevice = useMediaQuery(`( min-width: ${SCREEN.lg} )`)
+  return (
+    <div className='relative h-full lg:min-h-screen lg:h-fit w-full bg-primary-dark mx-auto'>
+      <Head>
+        <title>Neural Sight</title>
+        <meta name='description' content='Neural Sight an Ai medical system' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      {isLargeDevice || (
+        <header className='w-full h-fit'>
+          <SmallDeviceNavBar />
+        </header>
+      )}
+      <div className='h-full w-full flex justify-between '>
+        {isLargeDevice && <SideBar />}
+        {children}
+      </div>
+      {/* copyright */}
+      <footer className='absolute bottom-[0.6%] xl:bottom-[2.5%] flex w-full space-x-2 justify-center items-center'>
+        <p className=' text-sm lg:text-base text-slate-300 '>
+          &#169; Neurallabs Africa
+        </p>
+        <div className='rounded-full h-1 w-1 bg-white/80' />
+        <Link href={'#'}>
+          <p className='text-sm lg:text-base text-slate-300 cursor-pointer hover:text-slate-100 hover:underline hover:decoration-slate-100'>
+            Contact us
+          </p>
+        </Link>
+        <div className='rounded-full h-1 w-1 bg-white/80' />
+        <Link href={'#'}>
+          <p className='text-sm lg:text-base text-slate-300 cursor-pointer hover:text-slate-100 hover:underline hover:decoration-slate-100'>
+            Terms & privacy
+          </p>
+        </Link>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
