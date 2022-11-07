3cdae5fb352117c2a1d1cf557fb6efc226fa5679
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-07 17:32:24 +0300

    settings component

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,7 +1,20 @@
+import PatientIDSection from '../../components/dashboard/PatientIdSection'
+import MainContentSection from '../../components/dashboard/MainContentSection'
+import Layout from '../layout'
+
 type Props = {}
 
 function settings({}: Props) {
-  return <div>settings</div>
+  return (
+    <Layout>
+      <main className='w-full h-full flex pt-6 pb-10 justify-evenly'>
+        <section className='hidden lg:block w-full lg:w-auto xl:w-auto'>
+          <PatientIDSection />
+        </section>
+        <section className='w-full lg:w-[55%] xl:w-[70%]'>settings</section>
+      </main>
+    </Layout>
+  )
 }
 
 export default settings
