e7b0952bc452b483fd93c66e49a298d1c516bbb9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-08 16:54:27 +0300

    main section navbar wrapper

---
+++ components/MainSectionNavBar.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+
+const MainSectionNavBar = ({ children }: Props) => {
+  return (
+    <nav className='px-4 py-2 w-full h-fit bg-gray-50/95 lg:rounded-2xl flex justify-between lg:border-2 border-primary-light items-center'>
+      {children}
+    </nav>
+  )
+}
+
+export default MainSectionNavBar
