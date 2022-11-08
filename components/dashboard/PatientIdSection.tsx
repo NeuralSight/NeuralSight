2b14dd3b22da004ebe5ae473c51a6f80fefcad61
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-08 16:59:13 +0300

    used the listwrapper and listNavigation card

--- components/dashboard/PatientIdSection.tsx
+++ components/dashboard/PatientIdSection.tsx
@@ -1,7 +1,8 @@
 import { Icon } from '@iconify/react'
-import React from 'react'
+import { useState } from 'react'
 import InputField from '../Input'
-import PatientIdCard from './PatientIdCard'
+import ListNavigationWrapper from '../ListNavigationWrapper'
+import PatientIdCard from '../ListNavigationCard'
 
 type Props = {}
 
@@ -25,62 +26,62 @@ const patientIDs = [
 ]
 
 function PatientIdSection({}: Props) {
-  const [active, setActive] = React.useState<number>(0)
+  const [active, setActive] = useState<number>(0)
   return (
-    <div className='w-full h-full lg:border-2 lg:border-primary-light lg:rounded-2xl bg-gray-50/95 py-5 flex flex-col space-y-6 '>
-      <div className='flex flex-col space-y-6 px-3'>
-        <InputField
-          id='search'
-          type='text'
-          placeholder='Search Patient Id...'
-          size='small'
-          label='search'
-          borderRadius='25px'
-          icon={
-            <Icon
-              icon='ep:search'
-              className='cursor-pointer w-6 h-6 fill-current text-zinc-500/50'
-            />
-          }
-        />
-        <InputField
-          id='search'
-          type='text'
-          size='small'
-          label='New ID'
-          borderRadius='25px'
-          icon={
-            <button
-              type='submit'
-              className='absolute rounded-[25px] bg-gray-500/70 group-focus:group-hover:bg-primary-light flex justify-center items-center p-1.5 right-1 transition-all duration-200'
-              title='add patient'
-            >
+    <ListNavigationWrapper
+      title='patient&#39;s id'
+      topComponent={
+        <div className='flex flex-col space-y-6 px-3'>
+          {' '}
+          <InputField
+            id='search'
+            type='text'
+            placeholder='Search Patient Id...'
+            size='small'
+            label='search'
+            borderRadius='25px'
+            icon={
               <Icon
-                aria-label='add patient using Patients Id'
-                icon='akar-icons:person-add'
-                className=' cursor-pointer w-5 h-5 fill-current text-gray-50'
+                icon='ep:search'
+                className='cursor-pointer w-6 h-6 fill-current text-zinc-500/50'
               />
-            </button>
-          }
-        />
-      </div>
-      <div className='flex flex-col space-y-6 '>
-        <div className='uppercase text-lg text-center underline underline-offset-2'>
-          patient&#39;s id
+            }
+          />
+          <InputField
+            id='search'
+            type='text'
+            size='small'
+            label='New ID'
+            borderRadius='25px'
+            icon={
+              <button
+                type='submit'
+                className='absolute rounded-[25px] bg-gray-500/70 group-focus:group-hover:bg-primary-light flex justify-center items-center p-1.5 right-1 transition-all duration-200'
+                title='add patient'
+              >
+                <Icon
+                  aria-label='add patient using Patients Id'
+                  icon='akar-icons:person-add'
+                  className=' cursor-pointer w-5 h-5 fill-current text-gray-50'
+                />
+              </button>
+            }
+          />
         </div>
-        <div className='flex flex-col space-y-3 overflow-y-scroll max-h-[480px] w-full scrollbar-thin scrollbar-thumb-primary-light scrollbar-track-primary-light/20 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scroll-smooth'>
-          {patientIDs.map((patientId, key) => (
-            <PatientIdCard
-              id={patientId}
-              key={key}
-              idKey={key}
-              active={key === active}
-              handleActive={setActive}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+      }
+    >
+      {patientIDs.map((patientId, key) => (
+        <PatientIdCard
+          key={key}
+          idKey={key}
+          active={key === active}
+          setActive={setActive}
+          className={`justify-center ${key === active ? 'font-semibold' : ''}`}
+        >
+          {patientId}
+        </PatientIdCard>
+      ))}
+    </ListNavigationWrapper>
   )
 }
 
