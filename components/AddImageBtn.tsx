07b2a2b38100fbaa4d1da597a4db08f72211a004
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-10 18:02:26 +0300

    used the same width and height as the other cards

--- components/AddImageBtn.tsx
+++ components/AddImageBtn.tsx
@@ -10,7 +10,7 @@ const AddImageBtn = ({ setOpen }: Props) => {
     <button
       type='button'
       onClick={() => setOpen(true)}
-      className='rounded-3xl w-full h-full border-2 border-dashed border-primary-light flex flex-col space-y-4 justify-center items-center '
+      className='rounded-3xl  h-[387px] lg:h-[300px] 2xl:h-[387px] w-full lg:w-[240px] 2xl:w-[282px] border-2 border-dashed border-primary-light flex flex-col space-y-4 justify-center items-center '
     >
       <Icon
         icon='fluent:add-24-filled'
