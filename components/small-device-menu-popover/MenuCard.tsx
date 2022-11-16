d32b36ffe7cb7bb64150243f88a40d0f29a4da63
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-16 16:47:39 +0300

    include link for small menu card

--- components/small-device-menu-popover/MenuCard.tsx
+++ components/small-device-menu-popover/MenuCard.tsx
@@ -1,4 +1,5 @@
 import { Icon } from '@iconify/react'
+import Link from 'next/link'
 
 type Props = {
   key: number
@@ -7,12 +8,14 @@ type Props = {
   link: string
 }
 
-const MenuCard = ({ icon, text }: Props) => {
+const MenuCard = ({ icon, text, link, key }: Props) => {
   return (
-    <div className='rounded-md flex w-full items-center space-x-1 text-slate-800 hover:text-slate-900 text-cnter px-2 py-2 active:bg-zinc-500/50'>
-      <Icon icon={icon} className='h-4 w-4 fill-current' />
-      <p className='text-sm'>{text}</p>
-    </div>
+    <Link href={link}>
+      <div className='rounded-md flex w-full items-center space-x-1 text-slate-800 hover:text-slate-900 text-cnter px-2 py-2 active:bg-zinc-500/50'>
+        <Icon icon={icon} className='h-4 w-4 fill-current' />
+        <p className='text-sm'>{text}</p>
+      </div>
+    </Link>
   )
 }
 
