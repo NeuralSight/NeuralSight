886012d610282965d4b24b56961216085b9e6f6b
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-16 16:46:16 +0300

    this file includes all the accepted country codes

---
+++ helper/countryCodes.ts
@@ -0,0 +1,22 @@
+export const CountryCodes = [
+  {
+    country: 'Kenya',
+    code: '254',
+    flag: `/kenyaFlag.svg`,
+  },
+  {
+    country: 'Tanzania',
+    code: '255',
+    flag: `/tanzaniaFlag.svg`,
+  },
+  {
+    country: 'Senegal',
+    code: '221',
+    flag: `/senegalFlag.svg`,
+  },
+  {
+    country: 'Rwanda',
+    code: '250',
+    flag: `/rwandaFlag.svg`,
+  },
+]
