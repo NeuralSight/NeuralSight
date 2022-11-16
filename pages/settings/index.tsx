364164084b0c324908047f38247f8ef2bd6f8852
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-16 18:14:01 +0300

    introduced setActive for use by both children

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,21 +1,24 @@
+import { useState } from 'react'
+
 import MainContentSection from '../../components/settings/MainContentSection'
 import SettingOptionSection from '../../components/settings/SettingOptionSection'
 import Layout from '../layout'
 type Props = {}
 
-function settings({}: Props) {
+function Settings({}: Props) {
+  const [active, setActive] = useState<number>(0)
   return (
     <Layout>
       <main className='w-full h-[94%] flex pt-6 justify-evenly'>
         <section className='hidden lg:block w-full lg:w-auto xl:w-auto'>
-          <SettingOptionSection />
+          <SettingOptionSection active={active} setActive={setActive} />
         </section>
         <section className='w-full lg:w-[55%] xl:w-[70%]'>
-          <MainContentSection />
+          <MainContentSection active={active} setActive={setActive} />
         </section>
       </main>
     </Layout>
   )
 }
 
-export default settings
+export default Settings
