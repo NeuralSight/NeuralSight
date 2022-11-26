924c636d3947b3b3e0c06d4e170d04f72d82ed50
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-26 15:39:03 +0300

    auth context provider

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import { createContext } from 'react'
 import { UserInfo, AuthContextType } from '../typings'
 
@@ -11,26 +11,41 @@ type Props = {
 }
 
 const AuthProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<string | null | undefined>(undefined)
+  const [isRemembered, setIsRemembered] = useState<boolean>(false)
+
+  // set user auth info
   const setUserAuthInfo = async (data: UserInfo) => {
+    // set user to state
+    setUser(data.access_token)
     //store in react-cookies of access later instead
-    // req to set cookie in the backend
     const response = await fetch('/api/login', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ user: data.access_token }),
+      body: JSON.stringify({ user: data.access_token, remember: isRemembered }),
     })
     console.log('status', await response.json())
-    // if one logs in without setting remember me to true remember me the the cookies will be store for also log as the session is on the destroyed otherwise it will be store with a time limit of 1 week
   }
+
+  // get user authication info
+  const getUserAuthInfo = async () => {
+    // send a request to read the cookie in the headers
+    const response = await fetch('/api/is-loggedIn')
+    const data = await response.json()
+    setUser(data)
+  }
+  console.log('user', user)
   // checks if the user is authenticated or not
-  const isUserAuthenticated = () => !!Cookies.get('user')
+  const isUserAuthenticated = () => !!user
 
   const SampleAuthContext: AuthContextType = {
-    authState: Cookies.get('user'),
+    authState: user,
     setAuthState: (userAuthInfo: UserInfo) => setUserAuthInfo(userAuthInfo),
+    setIsRemembered: setIsRemembered,
     isUserAuthenticated,
+    getAuthState: () => getUserAuthInfo(),
   }
   return (
     <AuthContext.Provider value={SampleAuthContext}>
