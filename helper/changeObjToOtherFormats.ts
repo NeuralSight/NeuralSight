01c65ea28120c38dbd6213520255dd102b8b4489
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-09 09:38:21 +0300

    obj format to right content type

---
+++ helper/changeObjToOtherFormats.ts
@@ -0,0 +1,39 @@
+import { AnyObject } from '../typings'
+
+/**
+ * Converts a JavaScript object into a string with URL-encoded form data.
+ *
+ * @param {object} data - The JavaScript object to convert.
+ * @returns {string} - A string with URL-encoded form data.
+ */
+export function changeObjToFormUrlencoded(obj: AnyObject): string {
+  let str = []
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]))
+      console.log(key + ' -> ' + obj[key])
+    }
+  }
+  return str.join('&')
+}
+
+/**
+ * Converts a JavaScript object with file and text data into a FormData object
+ * that can be used as the body of a multipart/form-data request.
+ *
+ * @param {object} data - The JavaScript object to convert.
+ * @returns {FormData} - The FormData object that can be used as the body of a
+ *   multipart/form-data request.
+ */
+export function changeObjToFormData(obj: AnyObject): FormData {
+  // Create a new FormData object
+  let formData = new FormData()
+
+  // Loop through the object and add each key/value pair to the FormData object
+  for (let key in obj) {
+    formData.append(key, obj[key])
+  }
+
+  // Return the FormData object
+  return formData
+}
