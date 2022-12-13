d0cefe9a7117039bf0722700e49661d47c4af5f4
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-14 00:03:33 +0300

    added animations effect

--- components/small-device-menu-popover/MenuCard.tsx
+++ components/small-device-menu-popover/MenuCard.tsx
@@ -4,14 +4,18 @@ import Link from 'next/link'
 type Props = {
   text: string
   icon: string
-  link: string
+  link?: string
+  isClicked?: boolean
 }
 
-const MenuCard = ({ icon, text, link }: Props) => {
+const MenuCard = ({ icon, text, link, isClicked = false }: Props) => {
   return (
-    <Link href={link}>
-      <div className='rounded-md flex w-full items-center space-x-1 text-slate-800 hover:text-slate-900 text-cnter px-2 py-2 active:bg-zinc-500/50'>
-        <Icon icon={icon} className='h-4 w-4 fill-current' />
+    <Link href={link || ''}>
+      <div className='rounded-md flex w-full items-center space-x-1 text-slate-800 hover:text-slate-900 text-center hover:bg-gray-200 px-2 py-2 active:bg-zinc-500/50'>
+        <Icon
+          icon={icon}
+          className={`h-4 w-4 fill-current ${isClicked && 'animate-bounceX'}`}
+        />
         <p className='text-sm'>{text}</p>
       </div>
     </Link>
