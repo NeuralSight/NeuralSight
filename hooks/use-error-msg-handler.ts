b223f4b254676978453370219d01b36fe203b2e8
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-14 09:47:21 +0300

    backend  error messages handling hook

---
+++ hooks/use-error-msg-handler.ts
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { CONNECTION_ERR_MSG } from '../lang/errorMessages'
+import { ErrorDetails } from '../typings'
+
+interface ErrorHandler {
+  setError: Dispatch<SetStateAction<string | null>>
+}
+
+export default function useErrorMsgHandler({ setError }: ErrorHandler) {
+  const [detail, setDetails] = useState<ErrorDetails[] | string | undefined>()
+  useEffect(() => {
+    if (detail && typeof detail == 'string') {
+      setError(detail)
+    } else if (detail && typeof detail != 'string') {
+      detail.forEach((element: ErrorDetails) => {
+        setError(element.msg)
+        console.log('type', element.type)
+        console.log('loc', element.loc)
+      })
+    } else {
+      console.log(CONNECTION_ERR_MSG)
+      // setError(CONNECTION_ERR_MSG)
+    }
+  }, [detail, setError])
+  return { setDetails, detail }
+}
