5df77330514c6991c1ee29629dcc2c6eae3433c9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-14 09:47:33 +0300

    user logout hook

---
+++ hooks/use-logout.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+async function logoutRequest(url: string) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    return data
+  } catch (err: any) {
+    return { error: err }
+  }
+}
+
+export default function useLogout(url: string) {
+  const [isLoggingOut, setIsLoggingOut] = useState<boolean>(false)
+  const [isSuccess, setIsSuccessfull] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const logout = async () => {
+    setIsLoggingOut(true)
+    const data = await logoutRequest(url)
+    if (data.success) {
+      setIsLoggingOut(false)
+      setIsSuccessfull(true)
+    } else {
+      setError(data.error)
+      setIsSuccessfull(false)
+    }
+  }
+
+  return { isLoggingOut, error, logout, isSuccess }
+}
