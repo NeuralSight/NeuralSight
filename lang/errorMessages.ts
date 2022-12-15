c19b49a9817a996cc7973f64cc61fb58195a7b33
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-15 18:33:26 +0300

    method not allowed message

--- lang/errorMessages.ts
+++ lang/errorMessages.ts
@@ -6,6 +6,9 @@ const EMAIL_REQUIRED_ERR_MSG = 'email is required'
 const EMAIL_INCORRECT_ERR_MSG = 'email is not correct'
 const SERVER_ERR_MSG = 'oops, something went wrong with the server, try again'
 const CONNECTION_ERR_MSG = 'failed to connect, try again!'
+
+// api errors
+const METHOD_NOT_ALLOWED_ERR_MSG = 'Method Not Allowed'
 export {
   PASSWORD_REQUIRED_ERR_MSG,
   PASSWORD_LENGTH_ERR_MSG,
@@ -15,4 +18,5 @@ export {
   EMAIL_REQUIRED_ERR_MSG,
   SERVER_ERR_MSG,
   CONNECTION_ERR_MSG,
+  METHOD_NOT_ALLOWED_ERR_MSG,
 }
