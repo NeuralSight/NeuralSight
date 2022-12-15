7395f7dcb7af820c60560fff02772a63c2fa02cf
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-15 18:36:07 +0300

    image upload logic from next api

--- pages/api/image-upload.ts
+++ pages/api/image-upload.ts
@@ -1,13 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { METHOD_NOT_ALLOWED_ERR_MSG } from '../../lang/errorMessages'
+import { predictPatientImage } from '../../services/patient-api'
 
-type Data = {
-  name: string
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method == 'POST') {
+    try {
+      const { cookies } = req
+      const api_token = cookies.user || ''
+
+      const response = await predictPatientImage(req, res, api_token)
+      // console.log('data', data)
+      const data = await response.json()
+      res.status(response.status).json(data)
+      if (!response.ok) {
+        throw new Error(`unexpected response ${response.statusText}`)
+      }
+    } catch (e: any) {
+      console.log(e)
+      res.status(500).json({ detail: e.message })
+    }
+  } else {
+    return res.status(405).json({ detail: METHOD_NOT_ALLOWED_ERR_MSG })
+  }
 }
