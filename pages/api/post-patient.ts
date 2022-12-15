d4d28dc79e5eb323743cd8dbbfc576345d671812
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-15 18:36:32 +0300

    post patient logic

---
+++ pages/api/post-patient.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { METHOD_NOT_ALLOWED_ERR_MSG } from '../../lang/errorMessages'
+import { postPatient } from '../../services/patient-api'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method == 'POST') {
+    const { cookies } = req
+    const api_token = cookies.user
+    const patientId = JSON.parse(req.body)
+    const response = await postPatient({ token: api_token || '', patientId })
+    const data = await response.json()
+    res.status(response.status).json(data)
+  } else {
+    return res.status(405).json({ detail: METHOD_NOT_ALLOWED_ERR_MSG })
+  }
+}
