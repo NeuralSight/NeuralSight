e2744040d57a1d84bad29f91e8ad9426c368f62d
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-19 18:59:37 +0300

    Update index.tsx
    added update and get logic of user info

--- components/settings/edit-profile/index.tsx
+++ components/settings/edit-profile/index.tsx
@@ -1,30 +1,141 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, MouseEvent, useState } from 'react'
 import { CountryCodes } from '../../../helper/countryCodes'
-import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import Profile from './EditProfileImage'
 import InputField from '../../inputs/CustomInput'
 import Button from '../../Button'
 import Title from '../Title'
-import { EMAIL_INCORRECT_ERR_MSG } from '../../../lang/errorMessages'
+import {
+  EMAIL_INCORRECT_ERR_MSG,
+  MAX_DIGIT_TEL_NO_ERR_MSG,
+  TEL_INCORRECT_ERR_MSG,
+} from '../../../lang/errorMessages'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { fetchUserInfo, updateUser } from '../../../utils/config'
+import { NewUser, User } from '../../../typings'
+import useErrorMsgHandler from '../../../hooks/use-error-msg-handler'
 
 type Props = {}
 
 const EditProfile = (props: Props) => {
+  let firstname = ''
+  let lastname = ''
+
   const [countryCode, setCountryCode] = useState<string>(CountryCodes[0].code)
   const [isClicked, setIsClicked] = useState<boolean>(false)
+  const [profileImage, setProfileImage] = useState<File | undefined>()
+  console.log('profileImage', profileImage)
+  const [success, setSuccess] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const { setDetails } = useErrorMsgHandler({ setError })
+
   const handleCountryCode = (e: ChangeEvent<HTMLSelectElement>) => {
     setCountryCode(e.target.value)
+  }
+  const { data } = useQuery(
+    ['user'],
+    async () => (await fetchUserInfo()) as User
+  )
+
+  if (data) {
+    const names = data?.full_name.split(' ')
+
+    if (names?.length == 1) {
+      firstname = names[0]
+    } else {
+      if (names) {
+        firstname = names[0]
+        lastname = names[1]
+      }
+    }
   }
   const {
     control,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<FieldValues>()
+  } = useForm<NewUser>()
 
-  console.log('errors', errors)
-  const onSubmit: SubmitHandler<FieldValues> = (data) =>
-    console.log('data', data)
+  const { isLoading, mutate, status } = useMutation(updateUser, {
+    onMutate: async (newUser: User) => {
+      console.log('newPatient from uploading file', newUser)
+    },
+  })
+
+  const onSubmit: SubmitHandler<NewUser> = (newData: NewUser) => {
+    let newTel = newData.phone
+    let new_firstname = newData.firstname
+    let new_lastname = newData.lastname
+    let email = newData.email
+    let address = newData.address
+    let hospital = newData.hospital
+    let location = newData.location
+
+    // check what is the  country code
+    if (newTel == '' && data) {
+      newTel = data?.phone
+    } else {
+      newTel = '+' + countryCode + newData.phone
+    }
+    if (!new_firstname) {
+      new_firstname = firstname
+    }
+    if (!new_lastname) {
+      new_lastname = lastname
+    }
+    if (!email && data) {
+      email = data?.email
+    }
+    if (!location && data) {
+      location = data?.location
+    }
+    if (!address && data) {
+      address = data?.address
+    }
+    if (!hospital && data) {
+      hospital = data?.hospital
+    }
+
+    const UpdatedUser: User = {
+      userProfile: profileImage,
+      full_name: `${new_firstname} ${new_lastname}`,
+      email,
+      address,
+      phone: newTel,
+      location,
+      hospital,
+    }
+    mutate(UpdatedUser, {
+      onSuccess: async (response, variable, context) => {
+        const data = await response.json()
+        if (response.status === 201 || response.status === 200) {
+          console.log('data', data)
+          setSuccess('Successfully updated your information')
+          // currentClient.invalidateQueries('patient')
+        } else {
+          const detail = data.detail
+          console.log('detail', detail)
+          setDetails(detail)
+        }
+      },
+      onError: async (err: any, variables, context) => {
+        // currentClient.setQueryData('patient', context.previousPatients)
+        setError(err)
+        console.log('Error while posting...', err)
+        console.log('data sent is', variables)
+      },
+      onSettled: async () => {
+        // currentClient.invalidateQueries('patient')
+      },
+    })
+  }
+
+  // handle Cancel Update
+  const handleCancelUpdate = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    // cancel mutation
+  }
+
   return (
     <form
       className='w-full xl:w-3/4 flex flex-col space-y-8 '
@@ -32,17 +143,33 @@ const EditProfile = (props: Props) => {
     >
       <div className='flex justify-between'>
         <Title>Edit Profile</Title>
-        <Profile />
+
+        <Profile
+          profileImage={profileImage}
+          setProfileImage={setProfileImage}
+        />
         <div className='hidden xl:block' />
       </div>
+      {error && (
+        <p className='text-base text-red-500 bg-red-500/10 py-2 px-2 rounded-lg font-medium'>
+          {error}
+        </p>
+      )}
+      {success && (
+        <p className='text-base text-green-500 bg-green-500/10 py-2 px-2 rounded-lg font-medium'>
+          {success}
+        </p>
+      )}
       <div className='grid gap-2 grid-cols-1 md:grid-cols-2 h-fit'>
         <InputField
           type='text'
           label='firstname'
           fieldName='firstname'
           spaceY='2'
-          placeholder='John'
+          placeholder={firstname || 'John'}
           control={control}
+          defaultValue={firstname}
+          error={errors.firstname}
           rules={{
             // pattern:
             max: {
@@ -56,8 +183,10 @@ const EditProfile = (props: Props) => {
           label='lastname'
           fieldName='lastname'
           spaceY='2'
-          placeholder='Doe'
+          placeholder={lastname || 'Doe'}
           control={control}
+          defaultValue={lastname}
+          error={errors.lastname}
           rules={{
             // pattern:
             max: {
@@ -73,7 +202,9 @@ const EditProfile = (props: Props) => {
         fieldName='email'
         spaceY='2'
         control={control}
-        placeholder='example@gmail.com'
+        defaultValue={data?.email || ''}
+        error={errors.email}
+        placeholder={data?.email || 'example@gmail.com'}
         rules={{
           pattern: {
             value: /^\S+@\S+$/i,
@@ -89,7 +220,9 @@ const EditProfile = (props: Props) => {
         <select
           id='countrycode'
           value={countryCode}
-          className='appearance-none outline-none bg-transparent absolute top-1/2 transform translate-y-[18%] left-6 cursor-pointer'
+          className={`appearance-none outline-none bg-transparent absolute top-1/2 transform left-6 cursor-pointer ${
+            errors.phone ? '-translate-y-[40%]' : 'translate-y-[18%]'
+          }`}
           onChange={handleCountryCode}
           onMouseDown={() => setIsClicked(true)}
           onMouseLeave={() => setIsClicked(false)}
@@ -111,18 +244,24 @@ const EditProfile = (props: Props) => {
           type='tel'
           max={9}
           label='telephone'
-          fieldName='telephone'
+          fieldName='phone'
           spaceY='2'
-          placeholder='0720000000'
+          error={errors.phone}
+          placeholder={data?.phone || '720000000'}
           control={control}
+          defaultValue={data?.phone || ''}
           rules={{
-            pattern: {
-              value: /^[0-9]{10}/,
-              message: 'wrong telephone number',
-            },
-            max: {
+            maxLength: {
               value: 9,
-              message: 'max length 9',
+              message: MAX_DIGIT_TEL_NO_ERR_MSG,
+            },
+            minLength: {
+              value: 9,
+              message: MAX_DIGIT_TEL_NO_ERR_MSG,
+            },
+            pattern: {
+              value: /^[0-9]/,
+              message: TEL_INCORRECT_ERR_MSG,
             },
           }}
           className='pl-36'
@@ -134,7 +273,9 @@ const EditProfile = (props: Props) => {
           label='address'
           fieldName='address'
           spaceY='2'
-          placeholder='Jenga Leo, WestPark Tower'
+          defaultValue={data?.address || ''}
+          error={errors.address}
+          placeholder={data?.address || 'Jenga Leo, WestPark Tower'}
           control={control}
           rules={
             {
@@ -146,8 +287,10 @@ const EditProfile = (props: Props) => {
           type='text'
           label='location'
           fieldName='location'
+          error={errors.location}
           spaceY='2'
-          placeholder='Nairobi'
+          placeholder={data?.location || 'Nairobi'}
+          defaultValue={data?.location || ''}
           control={control}
           rules={
             {
@@ -161,7 +304,9 @@ const EditProfile = (props: Props) => {
         label='Hospital / Medical Institution'
         spaceY='2'
         fieldName='hospital'
-        placeholder='Nairobi Hospital'
+        error={errors.hospital}
+        defaultValue={data?.hospital || ''}
+        placeholder={data?.hospital || 'Nairobi Hospital'}
         control={control}
         rules={
           {
@@ -170,8 +315,10 @@ const EditProfile = (props: Props) => {
         }
       />
       <div className='grid gap-2 grid-cols-1 md:grid-cols-2 h-fit pb-3'>
-        <Button type='submit'>Save Changes</Button>
-        <Button type='button' outlined>
+        <Button type='submit'>
+          {isLoading ? 'Saving...' : 'Save Changes'}
+        </Button>
+        <Button type='button' outlined onClick={handleCancelUpdate}>
           Cancel
         </Button>
       </div>
