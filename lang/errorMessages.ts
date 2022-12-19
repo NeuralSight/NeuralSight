41190a2d52b38f58688b635f676c4abc6684983f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-19 19:00:59 +0300

    udpated lang
    added  METHOD_NOT_ALLOWED_ERR_MSG and   METHOD_NOT_ALLOWED_ERR_MSG

--- lang/errorMessages.ts
+++ lang/errorMessages.ts
@@ -6,9 +6,12 @@ const EMAIL_REQUIRED_ERR_MSG = 'email is required'
 const EMAIL_INCORRECT_ERR_MSG = 'email is not correct'
 const SERVER_ERR_MSG = 'oops, something went wrong with the server, try again'
 const CONNECTION_ERR_MSG = 'failed to connect, try again!'
+const TEL_INCORRECT_ERR_MSG =
+  'incorrect telephone number MUST be nine digits and numbers only'
+const MAX_DIGIT_TEL_NO_ERR_MSG = 'max length 9 digits after country code'
 
 // api errors
-const METHOD_NOT_ALLOWED_ERR_MSG = 'Method Not Allowed'
+const METHOD_NOT_ALLOWED_ERR_MSG = 'Method Not Allowed!'
 export {
   PASSWORD_REQUIRED_ERR_MSG,
   PASSWORD_LENGTH_ERR_MSG,
@@ -18,5 +21,7 @@ export {
   EMAIL_REQUIRED_ERR_MSG,
   SERVER_ERR_MSG,
   CONNECTION_ERR_MSG,
+  MAX_DIGIT_TEL_NO_ERR_MSG,
+  TEL_INCORRECT_ERR_MSG,
   METHOD_NOT_ALLOWED_ERR_MSG,
 }
