8f5d6e22477e272fd37e683abebfa471fa4a9b73
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-20 23:03:55 +0300

    Create report-context.tsx
    This is the context of te patient report data

---
+++ context/report-context.tsx
@@ -0,0 +1,77 @@
+import { useQuery } from '@tanstack/react-query'
+import { ReactNode, createContext, useState } from 'react'
+import { fetchPatientReport } from '../utils/config'
+import {
+  ErrorDetails,
+  PatientInfoData,
+  PatientReportResult,
+  ReportContextType,
+} from '../typings'
+import useErrorMsgHandler from '../hooks/use-error-msg-handler'
+// getting  report details on event trigger
+// set report id one should be able to access the report
+//set current active report
+// set the index of the report
+type Props = {
+  children: ReactNode
+}
+
+type Data = {
+  patient: PatientInfoData
+  'patient report': PatientReportResult[]
+}
+
+const ReportContext = createContext<null | ReportContextType>(null)
+const Provider = ReportContext.Provider
+
+const ReportProvider = ({ children }: Props) => {
+  const [patientId, setPatientId] = useState<string>('')
+  const [error, setError] = useState<string | null>(null)
+  const { detail, setDetails } = useErrorMsgHandler({ setError })
+  const [currentId, setSetCurrentId] = useState<number>(0)
+  const query = useQuery(
+    ['patients', patientId],
+    async () =>
+      (await fetchPatientReport(patientId || '')).json() as Promise<Data>,
+    {
+      onSuccess: (data) => {},
+      onError: (error: ErrorDetails[] | undefined) => {
+        // setError
+        setDetails(error)
+      },
+    }
+  )
+
+  const getAllReport = (): PatientReportResult[] => {
+    const patientReportArray: PatientReportResult[] =
+      query.data?.['patient report'] || []
+    return patientReportArray
+  }
+  // fetch report depending on the patient id set
+  const getReportByKey = () => {
+    const report: PatientReportResult | undefined = getAllReport().find(
+      (item: PatientReportResult, key) => key == currentId
+    )
+
+    return report
+  }
+
+  return (
+    <Provider
+      value={{
+        error,
+        detail,
+        setSetCurrentId,
+        getAllReport,
+        getReportByKey,
+        setPatientId,
+        isLoading: query.isLoading,
+        isSuccess: query.isSuccess,
+        isError: query.isError,
+      }}
+    >
+      {children}
+    </Provider>
+  )
+}
+export { ReportContext, ReportProvider }
