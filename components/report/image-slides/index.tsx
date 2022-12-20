242a3e90c5ebf7739f1d1aa1b2f75b0d500c8a0e
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-20 23:20:20 +0300

    Added logic for changing report data on slide change

--- components/report/image-slides/index.tsx
+++ components/report/image-slides/index.tsx
@@ -1,6 +1,6 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
-import React from 'react'
+import React, { useContext } from 'react'
 import 'swiper/css'
 import 'swiper/css/effect-cards'
 
@@ -10,23 +10,34 @@ import { EffectCards } from 'swiper'
 // get example image data
 import { SampleImagesArr } from '../../dashboard/MainContentSection'
 import ImageCard from './ImageCard'
+import { ReportContext } from '../../../context/report-context'
 
 type Props = {}
 
 const ImageSlides = (props: Props) => {
+  const reportContext = useContext(ReportContext)
+  const reportAll = reportContext?.getAllReport()
+
   return (
     <Swiper
       effect={'cards'}
       grabCursor={true}
       modules={[EffectCards]}
       className='mySwiper'
-      onSlideChange={(swiper) => console.log('changed', swiper.snapIndex)}
+      onSlideChange={(swiper) => {
+        reportContext?.setSetCurrentId(swiper.snapIndex)
+        return console.log('changed', swiper.snapIndex)
+      }}
     >
-      {SampleImagesArr.map((item, key) => (
-        <SwiperSlide key={key + item.patientID}>
-          <ImageCard imageDetails={item} />
-        </SwiperSlide>
-      ))}
+      {reportAll &&
+        reportAll?.length > 0 &&
+        reportAll?.map((item, key) => (
+          <SwiperSlide key={key}>
+            <ImageCard
+              imageDetails={SampleImagesArr[key % SampleImagesArr.length]}
+            />
+          </SwiperSlide>
+        ))}
     </Swiper>
   )
 }
