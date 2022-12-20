bed50c9f2b57576e5ec292c646fd4b85437fe64e
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-20 23:03:18 +0300

    Update datesFormatter.ts
    added formatting date string

--- helper/datesFormatter.ts
+++ helper/datesFormatter.ts
@@ -1,5 +1,28 @@
 export function formatDate(date: Date) {
-  const dateArr = date.toString().split(' ')
-  let formattedDate = `${dateArr[1]}/${dateArr[2]}/${dateArr[3]} ${dateArr[4]}`
-  return formattedDate
+  try {
+    const dateArr = date.toString().split(' ')
+    let formattedDate = `${dateArr[1]}/${dateArr[2]}/${dateArr[3]} ${dateArr[4]}`
+    return formattedDate
+  } catch (error) {
+    console.error('Could not format Error -', error)
+  }
+}
+
+export function formatDateFromString(date: string) {
+  // 2022-12-20T14:10:10.875246
+  try {
+    let dateArr = date.split('T')
+    const theDate = dateArr[0]
+    const time = dateArr[1]
+    dateArr = theDate.split('-')
+    let timeArr = time.split(':')
+    const hours = timeArr[0]
+    const min = timeArr[1]
+    const sec = parseInt(timeArr[2]).toFixed(0)
+
+    let formattedDate = `${dateArr[0]}/${dateArr[1]}/${dateArr[2]} ${hours}:${min}:${sec}`
+    return formattedDate
+  } catch (error) {
+    console.error('Could not format Error -', error)
+  }
 }
