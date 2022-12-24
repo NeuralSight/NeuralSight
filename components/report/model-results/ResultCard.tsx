8344c5d5a014e1c349f451b2e737b34fa171f1c8
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-12-24 16:07:52 +0300

    Update ResultCard.tsx
    added a not sure check this should be change dependeing with what is thought to be correct

--- components/report/model-results/ResultCard.tsx
+++ components/report/model-results/ResultCard.tsx
@@ -9,7 +9,12 @@ type Props = {
 
 const ResultCard = (props: Props) => {
   const result =
-    formatStringDecimalToPercentage(props.confidence) >= '75' ? 'YES' : 'NO'
+    formatStringDecimalToPercentage(props.confidence) >= '75'
+      ? 'YES'
+      : formatStringDecimalToPercentage(props.confidence) >= '50' &&
+        formatStringDecimalToPercentage(props.confidence) <= '74'
+      ? 'NOT SURE'
+      : 'NO'
   return (
     <div className='flex w-full pl-8 pr-2 items-center'>
       <p className='print:font-normal font-medium text-base text-zinc-800 capitalize tracking-wide'>
@@ -18,7 +23,11 @@ const ResultCard = (props: Props) => {
       <div className='h-0.5 rounded-full w-full bg-zinc-800/90 mx-2'></div>
       <p
         className={`print:font-medium font-bold uppercase tracking-wide ${
-          result != 'YES' ? 'text-green-500' : 'text-red-500'
+          result == 'NO'
+            ? 'text-green-500'
+            : result == 'NOT SURE'
+            ? 'text-orange-500'
+            : 'text-red-500'
         }`}
       >
         {props.showPercentage
