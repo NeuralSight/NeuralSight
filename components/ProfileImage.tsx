c89f7d39c769da301b4bb654a65d86aef65035b5
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-02-12 11:27:16 +0300

    added patient profile image data

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
-import React from 'react'
+import { use, useContext } from 'react'
+import { UserContext } from '../context/user-context'
+import { UserContextType } from '../typings'
+import DefaultProfile from '../public/user-solid.svg'
 
 type Props = {
   name?: string
@@ -7,14 +10,28 @@ type Props = {
 }
 
 function Profile({ name, photo }: Props) {
+  let imageType = ''
+  let fileName = ''
+  const userContext = useContext<UserContextType | null>(UserContext)
+  const userProfile = userContext?.getUserInfo()?.userProfile
+  console.log('userProfile', userProfile)
+  if (userProfile) {
+    if (typeof userProfile == 'string') {
+      const userProfileArr = userProfile.split('/')
+      imageType = userProfileArr[1]
+      fileName = userProfileArr[2]
+    }
+  }
+
   return (
     <div className='flex flex-col space-x-2'>
       <div>
         <Image
           className='relative h-12 w-12 lg:h-16 lg:w-16 rounded-full border-2 border-primary-light'
           src={
-            photo ||
-            'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80'
+            imageType && fileName
+              ? `${process.env.NEXT_PUBLIC_NEURALSIGHT_API_BASE_URL}/patient/file/${imageType}/${fileName}`
+              : DefaultProfile
           }
           width={100}
           height={100}
