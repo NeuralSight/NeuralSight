1d70c2f112d949cb07bc7dac153a1b78ba724723
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-02-22 20:54:51 +0300

    added fetch for the access token directly

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import cookie from 'cookie'
 import { METHOD_NOT_ALLOWED_ERR_MSG } from '../../lang/error-messages'
+import { loginUser } from '../../services/auth-api'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     res.setHeader(
       'Access-Control-Allow-Origin',
@@ -12,10 +16,18 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     )
+    // allows a server to identify referring pages that people are visiting from or where requested resources are being used
+    res.setHeader('Location', req.headers.referer || '/')
+
     if (req.method == 'POST') {
+      console.log('user', req.body.user)
+
+      const response = await loginUser(req.body.user)
+      const data = await response.json()
+      console.log('data', data)
       res.setHeader(
         'Set-Cookie',
-        cookie.serialize('user', req.body.user, {
+        cookie.serialize('user', data.access_token, {
           httpOnly: true, // only read in backend
           secure: process.env.NODE_ENV !== 'development', //secure while in production
           maxAge: req.body.remember ? 7 * 24 * 60 * 60 : 24 * 60 * 60, // max Age by default 1 day if remember is true then 1 week
@@ -25,9 +37,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       )
 
       // res.statusCode = 302
-      res.status(302).json({ sucess: true })
-      // allows a server to identify referring pages that people are visiting from or where requested resources are being used
-      res.setHeader('Location', req.headers.referer || '/')
+      res.status(response.status).json(data)
       // end the response process
       res.end()
     } else {
