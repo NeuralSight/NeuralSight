ae5d7113853c49abcc07d146232eb2810e437d0a
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-03-21 11:32:23 +0300

    added alert  on button signin with  google

--- components/auth/ThirdPartyBtn.tsx
+++ components/auth/ThirdPartyBtn.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 type Props = {
   children: React.ReactNode | string | undefined
   type: 'button' | 'submit' | 'reset' | undefined
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined
 }
-
 const ThirdPartyBtn = (props: Props) => {
   return (
     <button
+      onClick={props.onClick}
       type={props.type}
       className='flex w-full cursor-pointer max-h-6 py-6 px-20 rounded-xl bg-transparent border border-gray-500/50 text-sm lg:text-base font-medium text-zinc-500 justify-center items-center hover:bg-gray-400/10 transition-all duration-200 ease-in-out group outline-none hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-gray-500/50'
     >
