f67e2c16f6e158be7137c5ba98429dc0a76f1007
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-06-20 18:58:22 +0300

    remove file and  error handling  for get patien sget user and update user

--- pages/api/get-user.ts
+++ pages/api/get-user.ts
@@ -1,18 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { User } from '../../typings'
+import { Detail, User } from '../../typings'
 import { getUserImageReport } from '../../services/user-api'
 type Data = User
 
 export default async function getPatients(
   req: NextApiRequest,
-  res: NextApiResponse<User>
+  res: NextApiResponse<User | Detail>
 ) {
-  const { cookies } = req
-  const api_token = cookies.user
+  try {
+    const { cookies } = req
+    const api_token = cookies.user
 
-  const response = await getUserImageReport(api_token || '')
-  const data = (await response.json()) as Data
-  console.log('data', data)
-  res.status(response.status).json(data)
+    if (!api_token) {
+      res.status(403).json({ detail: `unauthorized access` })
+    }
+
+    const response = await getUserImageReport(api_token || '')
+    const data = (await response.json()) as Data
+    console.log('data', data)
+    res.status(response.status).json(data)
+  } catch (e: any) {
+    console.log(e)
+    if (e.statusCode) {
+      res.status(e.statusCode).json({ detail: e.message })
+    }
+    res.status(500).json({ detail: e.message })
+  }
 }
--- pages/api/update-user.ts
+++ pages/api/update-user.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { METHOD_NOT_ALLOWED_ERR_MSG } from '../../lang/error-messages'
 import { updateUserInfo } from '../../services/user-api'
+import { ApiError } from 'next/dist/server/api-utils'
 
 export const config = {
   api: {
@@ -18,16 +19,25 @@ export default async function handler(
       const { cookies } = req
       const api_token = cookies.user || ''
 
+      if (!api_token) {
+        res.status(403).json({ detail: `unauthorized access` })
+      }
+
       const response = await updateUserInfo(req, res, api_token)
       // console.log('data', data)
       const data = await response.json()
 
       res.status(response.status).json(data)
       if (!response.ok) {
-        throw new Error(`unexpected response ${response.statusText}`)
+        res
+          .status(400)
+          .json({ detail: `unexpected response ${response.statusText}` })
       }
     } catch (e: any) {
       console.log(e)
+      if (e.statusCode) {
+        res.status(e.statusCode).json({ detail: e.message })
+      }
       res.status(500).json({ detail: e.message })
     }
   } else {
